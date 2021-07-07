24332bd076aa16e7cf51423c36136a1c66195140
snelson82 <10576236+snelson82@users.noreply.github.com>
2021-07-07 00:09:59 -0600

    + updated markdown file names to be readme + updated day_1 script with file and directory checks

--- Daily Logs/Day 1/day_1.rb
+++ Daily Logs/Day 1/day_1.rb
@@ -1,13 +1,25 @@
+require 'byebug'
 require 'fileutils'
 
 SNIPPET = "# Summary\n\n## Topics Studied\n\n- topic\n\n## Resources Used\n\n[resource](resource_link)\n\n## Links to Projects & Notes\n\n[project_or_gist](project_or_gist_link)\n\n[Continue to Day #](/Daily%20Logs/Day #/day_#.md)".freeze
 
 def create_structure(arr)
   arr.each do |day|
-    FileUtils.mkdir_p("../Day #{day}") unless File.exist?("../Day #{day}")
-    File.open("../Day #{day}/day_#{day}.rb", 'w')
-    File.open("../Day #{day}/day_#{day}.md", 'w') { |file| file << SNIPPET }
+    daily_dir    = File.expand_path("Day #{day}", 'Daily Logs')
+    daily_rb     = File.expand_path("day_#{day}.rb", "Daily Logs/Day #{day}")
+    daily_readme = File.expand_path('README.md', "Daily Logs/Day #{day}")
+
+    FileUtils.mkdir_p(daily_dir) unless File.exist?(daily_dir)
+    unless File.exist?(daily_rb)
+      f1 = File.open(daily_rb, 'w')
+      f1.close
+    end
+
+    unless File.exist?(daily_readme)
+      f2 = File.open(daily_readme, 'w') { |file| file << SNIPPET }
+      f2.close
+    end
   end
 end
 
-create_structure((1..100).to_a)
+create_structure((4..100).to_a)
